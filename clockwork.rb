ee153aaf6d40f72a8d40d26b1334ba72c2fc7e87
Callum Jones <callum@callumj.com>
2014-03-14 00:01:10 +0800

    Output

--- clockwork.rb
+++ clockwork.rb
@@ -2,7 +2,9 @@ require 'clockwork'
 module Clockwork
 
   every(1.hour, 'process_latest') {
-    `rake process_latest`
+    IO.popen("rake process_latest").each do |line|
+      puts line.chomp
+    end
   }
   
 end
